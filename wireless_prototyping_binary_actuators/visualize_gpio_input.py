31ec7cd5cf3797807a7ba6e109e6fcf782b7a6c4
AnthonyZi_T430 <AnthonyZi@users.noreply.github.com>
2018-04-02 01:15:34 +0200

    small changes in visualisation (signal processing)

--- wireless_prototyping_binary_actuators/visualize_gpio_input.py
+++ wireless_prototyping_binary_actuators/visualize_gpio_input.py
@@ -1,7 +1,21 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as scisig
 
 if __name__ == "__main__":
     with open("gpio_input.txt", "r") as f:
         lines = f.readlines()
+    l = lines[0]
+    y = [0 if c == "0" else 1 for c in l]
 
-    print(lines)
+    y = np.array(y)
+
+    k_size = 100
+    kernel = np.ones(k_size)
+    y = scisig.convolve(y,kernel)
+    y = np.where(y>(k_size/2),1,0)
+
+    x = np.arange(y.shape[0])
+
+    plt.plot(x,y)
+    plt.show()
