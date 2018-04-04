501c1956c1e0046db6f6794f9c70189ae4ee3bf9
AnthonyZi_Pi <AnthonyZi@users.noreply.github.com>
2018-04-04 11:33:05 +0200

    gpio_read now receives 8 packages at once from elro transmitter if a signal consists of 12 bits these are printed, elsewise only the number of received bits are printed

--- wireless_prototyping_binary_actuators/AM_Receiving/gpio_am_read.c
+++ wireless_prototyping_binary_actuators/AM_Receiving/gpio_am_read.c
@@ -9,106 +9,169 @@ typedef struct smoothReceivers
 {
     uint8_t* data;
     uint8_t doff;
-    uint8_t* vals;
-    uint8_t voff;
+//    uint8_t* vals;
+//    uint8_t voff;
+    uint8_t current_level;
+    uint16_t counter;
+    uint16_t last_length;
 
     uint8_t window_size;
     uint8_t thresh;
 } SmoothReceiver;
 
-typedef struct dataInterpreters
-{
-    uint8_t* data;
-    uint8_t doff;
-
-    uint8_t* vals;
-    uint8_t voff;
-
-    uint8_t mode;
-    uint8_t longpause;
-} DataInterpreter;
 
 SmoothReceiver init_SmoothReceiver()
 {
     SmoothReceiver sr;
-//    sr.data = (uint8_t*)malloc(sizeof(uint8_t)*256);
     sr.data = calloc(256,sizeof(uint8_t));
     sr.doff = 0;
-//    sr.vals = (uint8_t*)malloc(sizeof(uint8_t)*256);
-    sr.vals = calloc(256,sizeof(uint8_t));
-    sr.voff = 0;
 
-    sr.window_size = 9;
-    sr.thresh = 5;
+    sr.current_level = 0;
+    sr.counter = 0;
+    sr.last_length = 0;
+
+    sr.window_size = 15;
+    sr.thresh = 8;
     return sr;
 }
 
-DataInterpreter init_DataInterpreter()
-{
-    DataInterpreter di;
-//    di.data = (uint8_t*)malloc(sizeof(uint8_t)*256);
-    di.data= calloc(256,sizeof(uint8_t));
-    di.doff = 0;
-//    di.vals = (uint8_t*)malloc(sizeof(uint8_t)*256);
-    di.vals = calloc(256,sizeof(uint8_t));
-    di.voff = 0;
 
-    di.mode = 0;
-    di.longpause = 0;
-    return di;
+typedef struct bitcodes
+{
+    uint8_t* data;
+    uint16_t length;
+} Bitcode;
+
+Bitcode init_Bitcode()
+{
+    Bitcode bc;
+    bc.data = calloc(10000,sizeof(uint8_t));
+    bc.length = 0;
+    return bc;
+}
+
+typedef struct signalcodes
+{
+    uint8_t* data;
+    uint16_t length;
+} Signalcode;
+
+Signalcode init_Signalcode()
+{
+    Signalcode sc;
+    sc.data = calloc(10000,sizeof(uint8_t));
+    sc.length = 0;
+    return sc;
 }
 
 
-void smoothRead(SmoothReceiver *sr, int pin)
+// reads data into a buffer and realises a fir filter to smoothen readings
+// fir is similar to a mean filter with a window_size from sr ..
+//   .. if sr.thresh = (window_size + 1)/2
+uint8_t smoothRead(SmoothReceiver *sr, int pin)
 {
-    *(sr->data+(sr->doff++)) = (uint8_t)digitalRead(pin);
+    *(sr->data+(sr->doff)) = (uint8_t)digitalRead(pin);
     int v = 0;
-    uint8_t p = sr->doff;
+    uint8_t p = sr->doff++;
     for(uint8_t i = 0; i<sr->window_size; i++)
     {
-        p = (uint8_t)(p-1);
         v += *((sr->data)+p);
+        p--;
     }
-    *((sr->vals)+(sr->voff++)) = v/sr->thresh;
+    uint8_t lvl = v/sr->thresh;
+    if(lvl == sr->current_level)
+    {
+        sr->counter++;
+        return 0;
+    }
+    else
+    {
+        sr->current_level = lvl;
+        sr->last_length = sr->counter;
+        sr->counter = 0;
+        return 1;
+    }
 }
 
-void read(int pin)
+Signalcode decode(Bitcode *bc)
+{
+    Signalcode sc = init_Signalcode();
+    for(uint16_t i = 1; i<bc->length; i+=2)
+        *(sc.data+sc.length++) = *(bc->data+i);
+    return sc;
+}
+
+Bitcode read(int pin)
 {
     SmoothReceiver sr = init_SmoothReceiver();
-    DataInterpreter di = init_DataInterpreter();
+    Bitcode bc = init_Bitcode();
     FILE *fp1;
-    FILE *fp2;
     fp1 = fopen("fp1", "wb");
-    fp2 = fopen("fp2", "wb");
 
-    int c = 0;
-    while(c<100000)
+    // wait until first level-change (low to high because SmoothReader is initialised with current_level = 0)
+    while(!smoothRead(&sr, pin));
+
+    uint8_t lvl_change;
+    uint16_t onlength;
+    uint8_t bitcounter = 0;
+    uint16_t breakpause = 1000;
+    while(1)
     {
-        c++;
-        smoothRead(&sr, pin);
-        uint8_t doff = sr.doff-1;
-        uint8_t voff = sr.voff-1;
-        fprintf(fp1, "%d ", *(sr.data+doff));
-        fprintf(fp2, "%d ", *(sr.vals+voff));
-        delayMicroseconds(10);
+        fprintf(fp1, "%d ", sr.current_level);
+        lvl_change = smoothRead(&sr, pin);
+        //if lvl_change read out how long level was hold
+        if(lvl_change)
+        {
+            if(bitcounter++ == 0)
+                onlength = sr.last_length;
+            else
+            {
+                *((bc.data)+(bc.length)) = onlength > sr.last_length;
+                breakpause = *((bc.data)+(bc.length++)) ? 4*onlength : 4*sr.last_length;
+            }
+            bitcounter %= 2;
+        }
+//        delayMicroseconds(5);
+
+        if(sr.counter > breakpause)
+            break;
     }
+
     fclose(fp1);
-    fclose(fp2);
 
     free(sr.data);
-    free(sr.vals);
-    free(di.data);
-    free(di.vals);
+    return bc;
 }
 
 int main(int argc, char** argv)
 {
     int pin = atoi(argv[1]);
-//    char* outfile = argv[2];
     wiringPiSetup();
     pinMode(pin, INPUT);
 
-    read(pin);
+    Bitcode *bcs = calloc(8,sizeof(Bitcode));
+    Signalcode *scs = calloc(8,sizeof(Signalcode));
+    while(1)
+    {
+        for(uint8_t i = 0; i<8; i++)
+            bcs[i] = read(pin);
+
+        for(uint8_t i = 0; i<8; i++)
+            scs[i] = decode(&(bcs[i]));
+
+        for(uint8_t i = 0; i<8; i++)
+        {
+            printf("%d - ", scs[i].length);
+            if(scs[i].length != 12)
+            {
+                printf("\n");
+                break;
+            }
+            for(uint16_t j = 0; j<scs[i].length; j++)
+                printf("%d", *((scs[i].data)+j));
+            printf("\n");
+        }
+    }
 
     return 0;
 }
