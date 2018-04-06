2c02c27e70fe93a67cb30097825411b1abd0eeca
AnthonyZi_T430 <AnthonyZi@users.noreply.github.com>
2018-04-06 17:36:59 +0200

    visualisation now plots all files with fp*

--- wireless_prototyping_binary_actuators/AM_Receiving/visualize_gpio_input.py
+++ wireless_prototyping_binary_actuators/AM_Receiving/visualize_gpio_input.py
@@ -2,29 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as scisig
 import sys
+import os
 
 if __name__ == "__main__":
 
-    f1 = "fp1"
-    f2 = "fp2"
+    files = [f for f in os.listdir(".") if "fp" in f]
+    files = sorted(files)
+    for f in files:
 
-    with open(f1, "r") as f:
-        lines1 = f.readlines()
-    with open(f2, "r") as f:
-        lines2 = f.readlines()
+        with open(f, "r") as ff:
+            lines = ff.readlines()
 
-    l1 = lines1[0].replace(" ", "")
-    l2 = lines2[0].replace(" ", "")
+        l = lines[0].replace(" ", "")
 
-    y1 = [0 if c == "0" else 1 for c in l1]
-    y2 = [0 if c == "0" else 1 for c in l2]
+        y = [0 if c == "0" else 1 for c in l]
 
-    y1 = np.array(y1)
-    y2 = np.array(y2)
+        y = np.array(y)
 
-    x1 = np.arange(y1.shape[0])
-    x2 = np.arange(y2.shape[0])
+        x = np.arange(y.shape[0])
 
-    plt.plot(x1,y1, 'g')
-    plt.plot(x2,y2, 'r')
-    plt.show()
+        print(f)
+        plt.figure()
+        plt.plot(x,y, 'g')
+        plt.show()
