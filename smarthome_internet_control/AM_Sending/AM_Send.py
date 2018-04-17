fe83bda2897a600a7d0658446a561ec8d1d4f943
AnthonyZi_Pi <AnthonyZi@users.noreply.github.com>
2018-04-17 14:37:05 +0200

    small bugfixes - smarthome-internet-controller works now!

--- smarthome_internet_control/AM_Sending/AM_Send.py
+++ smarthome_internet_control/AM_Sending/AM_Send.py
@@ -1,20 +1,22 @@
 import subprocess
 
 class AM_Sender(object):
-    def __init__(self, pin, device_id):
+    def __init__(self, pin, device_id, am_send_folder):
         self.pin = pin
         self.device_id = device_id
         self.id_signal = "0".join(self.device_id) + "0"
+        self.am_send_folder = am_send_folder
 
 
     def send(self, sig1, sig2, sig2_end):
-        args = ["./gpio_am_send", str(self.pin), self.id_signal, sig1, sig2, sig2_end]
+        executable = self.am_send_folder+"/gpio_am_send"
+        args = [executable, str(self.pin), self.id_signal, sig1, sig2, sig2_end]
         subprocess.call(args)
 
 
 class RSC_Sender(AM_Sender):
-    def __init__(self, pin, device_id):
-        super().__init__(pin, device_id)
+    def __init__(self, pin, device_id, am_send_folder):
+        super().__init__(pin, device_id, am_send_folder)
         self.sig1_dict = {'A':"0111", 'B':"1011", 'C':"1101", 'D':"1110"}
         self.sig2_dict = {'ON':"101", 'OFF':"110"}
         self.id_signal = "0" + self.id_signal
@@ -28,10 +30,11 @@ class RSC_Sender(AM_Sender):
 
 if __name__ == "__main__":
     import time
-    s1 = RSC_Sender(1, "10110")
-    s2 = RSC_Sender(1, "00000")
-    s1.send("B","ON")
+    s1 = RSC_Sender(1, "10110", ".")
+    s2 = RSC_Sender(1, "00000", ".")
+    s1.send("A","ON")
+    s1.send("A","ON")
     s2.send("A","ON")
     time.sleep(1)
-    s1.send("B","OFF")
+    s1.send("A","OFF")
     s2.send("A","OFF")
--- smarthome_internet_control/smarthomecontroller.py
+++ smarthome_internet_control/smarthomecontroller.py
@@ -1,16 +1,26 @@
 from smarthomeserver import *
 import queue
 from AM_Sending import AM_Send
+import time
 
 class SmartHomeControl(threading.Thread):
     def __init__(self,client_threads):
         super().__init__()
         self.instruction_queue = queue.Queue()
         self.client_threads = client_threads
+
+        self.state_change_wait_time = 2
+
+        self.state_change_time = dict()
         self.states = dict()
+
         self.states["light_main"] = "off"
         self.states["light_table"] = "off"
-        self.rsc1 = AM_Send.RSC_Sender(1, "10110")
+
+        self.state_change_time["light_main"] = 0
+        self.state_change_time["light_table"] = 0
+
+        self.rsc1 = AM_Send.RSC_Sender(1, "10110", "AM_Sending")
 
 
     def run(self):
@@ -24,7 +34,7 @@ class SmartHomeControl(threading.Thread):
         itype, idevice, istate = i_list[:3]
         if(itype == "set"):
             if(self.set_device_state(idevice, istate)):
-                self.signal_broadcast("update", idevice, istate)
+                self.signal_broadcast("update", idevice, self.states[idevice])
 
 
     def signal_broadcast(self, itype, idevice, istate):
@@ -35,18 +45,25 @@ class SmartHomeControl(threading.Thread):
 
     def set_device_state(self, idevice, istate):
         if(idevice in self.states.keys()):
-            if(idevice == "light_main"):
-                if(istate in ["on", "off"]):
-                    self.states[idevice] = istate
-                    rsc.send("A", istate.upper())
-                else:
-                    return False
-            elif(idevice == "light_table"):
-                if(istate in ["on", "off"]):
-                    self.states[idevice] = istate
-                    rsc.send("B", istate.upper())
-                else:
-                    return False
+            if(time.time()-self.state_change_time[idevice] > self.state_change_wait_time):
+                self.state_change_time[idevice] = time.time()
+                if(idevice == "light_main"):
+                    if(istate in ["on", "off"]):
+                        self.states[idevice] = istate
+                        self.rsc1.send("A", istate.upper())
+                        self.rsc1.send("A", istate.upper())
+                    else:
+                        return False
+                elif(idevice == "light_table"):
+                    if(istate in ["on", "off"]):
+                        self.states[idevice] = istate
+                        self.rsc1.send("B", istate.upper())
+                        self.rsc1.send("B", istate.upper())
+                    else:
+                        return False
+            else:
+                print("last set for {} was {} seconds ago".format(idevice, time.time()-self.state_change_time[idevice]))
+                return True
         else:
             return False
         return True
